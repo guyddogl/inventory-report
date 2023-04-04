3f26120a5f3558e3d02d9dddc1e644c55bfe09b3
Guyddo <guyddogl@gmail.com>
2023-04-04 18:27:30 -0300

    Requirement 8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        "01",
+        "Deco X60",
+        "TP-Link",
+        "04/04/2023",
+        "28/11/2023",
+        "134004042023",
+        "em ambiente refrigerado",
+    )
+
+    repr = product.__repr__()
+
+    result = (
+        "O produto Deco X60 fabricado em 04/04/2023"
+        " por TP-Link com validade até 28/11/2023"
+        " precisa ser armazenado em ambiente refrigerado."
+    )
+
+    assert repr == result
