3514ea637e76de00835d56d2b02089c08ed1f1ff
Guyddo <guyddogl@gmail.com>
2023-04-04 14:39:21 -0300

    Requirement 2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,32 @@
+from datetime import datetime
+from collections import Counter
+
+
+def oldest_manufacture(products):
+    return min([product["data_de_fabricacao"] for product in products])
+
+
+def closest_expiration(products):
+    list = []
+    for product in products:
+        data_de_validade = datetime.strptime(
+            product["data_de_validade"], "%Y-%m-%d"
+        )
+        if data_de_validade > datetime.now():
+            list.append(product["data_de_validade"])
+    return min(list)
+
+
+def comp_with_more_products(products):
+    company = [product["nome_da_empresa"] for product in products]
+    return Counter(company).most_common(1)[0][0]
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(products):
+        return (
+            f"Data de fabricação mais antiga: {oldest_manufacture(products)}\n"
+            f"Data de validade mais próxima: {closest_expiration(products)}\n"
+            f"Empresa com mais produtos: {comp_with_more_products(products)}"
+        )
