2369e7631aef947404a065cbf0505e81186c758e
Guyddo <guyddogl@gmail.com>
2023-04-04 13:43:45 -0300

    Requirement 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        "01",
+        "Deco X60",
+        "TP-Link",
+        "04/04/2023",
+        "não há",
+        "134004042023",
+        "Ambiente refrigerado",
+    )
+
+    assert product.id == "01"
+    assert product.nome_do_produto == "Deco X60"
+    assert product.nome_da_empresa == "TP-Link"
+    assert product.data_de_fabricacao == "04/04/2023"
+    assert product.data_de_validade == "não há"
+    assert product.numero_de_serie == "134004042023"
+    assert product.instrucoes_de_armazenamento == "Ambiente refrigerado"
