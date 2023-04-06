609a8373601e7f8b2d89611af838ff8845040f0f
Guyddo <guyddogl@gmail.com>
2023-04-05 22:22:18 -0300

    Requirements 10 and 11

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,30 @@
+import sys
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
+
 def main():
-    pass
+
+    if len(sys.argv) <= 2:
+        return print("Verifique os argumentos", file=sys.stderr)
+
+    _, path, type = sys.argv
+
+    data = ""
+
+    if path.endswith(".csv"):
+        data = InventoryRefactor(CsvImporter)
+    if path.endswith(".json"):
+        data = InventoryRefactor(JsonImporter)
+    if path.endswith(".xml"):
+        data = InventoryRefactor(XmlImporter)
+
+    result = data.import_data(path, type)
+
+    print(result, end="")
+
+
+if __name__ == "__main__":
+    main()
